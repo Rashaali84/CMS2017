c55d12f2411e361b42d74a38e9f871c4a5d8d53e
Rasha ali <rashaali84@gmail.com>
2020-04-03 20:47:06 +0200

    Add project files.

---
+++ CMS2017/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CMS2017.Startup))]
+namespace CMS2017
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
